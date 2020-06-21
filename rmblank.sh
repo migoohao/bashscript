3521735188e677f36ca6f2ee379d45ac2075e1db
migoo <migoo_hao@163.com>
2020-06-21 23:59:58 +0800

    rmblank
    Signed-off-by: migoo <migoo_hao@163.com>

---
+++ rmblank.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. ./utils.sh
+
+if [[ $# -eq 0 ]]; then
+    _red "Usage is $0 [filepath]+"
+    exit 1
+fi
+
+for file in $@ ; do
+    if [[ ! -f ${file} ]]; then
+        _red "${file} is not a file"
+        exit 2
+    fi
+done
+
+sed '/^$/d' -i $@
